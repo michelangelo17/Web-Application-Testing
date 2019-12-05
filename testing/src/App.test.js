59adb20cf2eace549df9ff55c9429bdc1df96d26
michelangelo17 <michelangelo.markus@gmail.com>
2019-12-04 22:02:04 -0800

    mvp components rendering properly, tests passing, no functionality yet

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders app without crashing', () => {
+  render(<App />)
+})
+
+test('contains the site title', () => {
+  const { getByText } = render(<App />)
+  getByText(/baseball counter/i)
+})
+
+test('should display number of balls', () => {
+  const { getByText } = render(<App />)
+  getByText(/balls/i)
+})
+
+test('should contain a button for foul', () => {
+  const { getByText } = render(<App />)
+  getByText(/foul/i)
+})
